e14834bb76454aed792016560e8e7832e90b6abc
hemaabokila <ibrahemabokila@gmail.com>
2024-09-30 18:38:08 -0400

    update ddos

---
+++ ddos/__init__.py
@@ -0,0 +1,5 @@
+from .main import main
+
+__version__ = "1.0.0"
+__author__ = "Ibrahem abo kila"
+__email__ = "ibrahemabokila@gmail.com"
--- setup.py
+++ setup.py
@@ -2,26 +2,27 @@ from setuptools import setup, find_packages
 
 setup(
     name="ddos_attack",
-    version="1.0.0", 
-    author="Ibrahem abo kila", 
-    author_email="ibrahemabokila@gmail.com",  
-    description="A simple DDoS attack simulation tool.", 
-    long_description=open('README.md').read(),  
-    long_description_content_type="text/markdown",  
-    url="https://github.com/hemaabokila/ddos_attack", 
-    packages=find_packages(), 
-    install_requires=[ 
-        "rich",  
+    version="1.0.0",
+    author="Ibrahem abo kila",
+    author_email="ibrahemabokila@gmail.com",
+    description="A simple DDoS attack simulation tool.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/hemaabokila/ddos_attack",
+    packages=find_packages(),
+    install_requires=[
+        "rich",
     ],
-    classifiers=[ 
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',  
-    entry_points={  
+    python_requires='>=3.6',
+    entry_points={
         'console_scripts': [
-            'ddos=ddos:main',  
+            'ddos=ddos.main:main', 
         ],
     },
 )
+
